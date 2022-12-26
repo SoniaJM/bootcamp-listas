c895dc881732ee317c57d330d4d7b9c1e4795c72
SoniaJM <smoreyra86@gmail.com>
2022-12-26 01:30:55 -0300

    primer

---
+++ index.js
@@ -0,0 +1,12 @@
+const Milista = [
+    "Sonia",
+     36, 
+     false, 
+     newDate(1986, 11, 14), 
+    {
+        titulo:"Yo antes de ti",
+        autor: "Jojo Moyes",
+        fecha: newDate(2014, 4, 1),
+        url: "https://www.tematika.com/yo-antes-de-ti-9789870434665.html"
+    },
+    ];
